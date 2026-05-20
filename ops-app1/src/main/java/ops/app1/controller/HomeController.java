0c2f471d7814bac0d8193a00e18fc895784f4893
zionn1002 <zionn1002@naver.com>
2026-05-20 09:43:23 +0900

    feat ops-app1 HomeController

--- ops-app1/src/main/java/ops/app1/controller/HomeController.java
+++ ops-app1/src/main/java/ops/app1/controller/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
     public String home(Model model) {
         model.addAttribute("appName", "ops-app1");
         model.addAttribute("profile", profile);
-        model.addAttribute("message", "Hello from ops-app1 v2 (" + profile + ")");
+        model.addAttribute("message", "Hello from ops-app1 v3 (" + profile + ")");
         return "home";
     }
 }
